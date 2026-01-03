7f8deefcd9fb9623fe27f39906485442fd2e3580
Junghwan Kim <everydayeffort@naver.com>
2026-01-03 15:11:52 +0900

    GUI End

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
 g++ main.cpp -o PEViewer_32bit.exe
+g++ main_gui.cpp -o "PEViewer(GUI)_32bit.exe" -lgdi32 -lcomdlg32 -mwindows
 pause
--- main_gui.cpp
+++ main_gui.cpp
@@ -2,6 +2,9 @@
 #include<commdlg.h>
 #include<string>
 #include<vector>
+#include<sstream>
+#include<iomanip>
+#include<fstream>
 using namespace std;
 #define ID_FILE_OPEN    1001
 #define ID_FILE_EXIT    1002
@@ -13,6 +16,23 @@ using namespace std;
 
 HWND hEdit;
 string cur_Filepath = "";
+
+typedef struct DLLThunks {
+    string dll_name;
+    DWORD int_rva;
+    DWORD iat_rva;
+    vector<IMAGE_THUNK_DATA32> int_thunks;
+    vector<IMAGE_THUNK_DATA32> iat_thunks;
+}DLLThunks;
+
+DWORD RVAToRAW(DWORD rva, vector<IMAGE_SECTION_HEADER>& section_headers){
+    for (auto& section : section_headers) {
+        if (rva >= section.VirtualAddress && rva < section.VirtualAddress + section.Misc.VirtualSize) {
+            return (rva - section.VirtualAddress) + section.PointerToRawData;
+        }
+    }
+    return 0;
+}
 
 bool OpenPE(HWND hwnd){
     OPENFILENAMEA ofn;
@@ -41,6 +61,289 @@ bool OpenPE(HWND hwnd){
     return false;
 }
 
+string GetDosHeaderString(string filepath){
+    ifstream fin(filepath, ios::binary);
+    if(!fin) return "Error : Cannot open file";
+    IMAGE_DOS_HEADER dos_header;
+    fin.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
+    fin.close();
+    if(dos_header.e_magic != IMAGE_DOS_SIGNATURE){
+        return "Error : Invalid PE File";
+    }
+    stringstream ss;
+    ss << "---------- DOS HEADER ----------" << "\r\n";
+    ss << hex << uppercase << setfill('0');
+    
+    ss << "Signature                    : " << setw(4) << dos_header.e_magic << "\r\n";
+    ss << "Bytes on Last Page of File   : " << setw(4) << dos_header.e_cblp << "\r\n";
+    ss << "Pages in File                : " << setw(4) << dos_header.e_cp << "\r\n";
+    ss << "Relocations                  : " << setw(4) << dos_header.e_crlc << "\r\n";
+    ss << "Size of Header in Paragraphs : " << setw(4) << dos_header.e_cparhdr << "\r\n";
+    ss << "Minimum Extra Paragraphs     : " << setw(4) << dos_header.e_minalloc << "\r\n";
+    ss << "Maximum Extra Paragraphs     : " << setw(4) << dos_header.e_maxalloc << "\r\n";
+    ss << "Initial SS                   : " << setw(4) << dos_header.e_ss << "\r\n";
+    ss << "Initial SP                   : " << setw(4) << dos_header.e_sp << "\r\n";
+    ss << "Checksum                     : " << setw(4) << dos_header.e_csum << "\r\n";
+    ss << "Initial IP                   : " << setw(4) << dos_header.e_ip << "\r\n";
+    ss << "Initial CS                   : " << setw(4) << dos_header.e_cs << "\r\n";
+    ss << "Offset to Relocation Table   : " << setw(4) << dos_header.e_lfarlc << "\r\n";
+    ss << "Overlay Number               : " << setw(4) << dos_header.e_ovno << "\r\n";
+    
+    for(int i = 0; i<4; i++){
+        ss << "Reserved[" << i << "]                 : " << setw(4) << dos_header.e_res[i] << "\r\n";
+    }
+    
+    ss << "OEM Identifier               : " << setw(4) << dos_header.e_oemid << "\r\n";
+    ss << "OEM Information              : " << setw(4) << dos_header.e_oeminfo << "\r\n";
+    
+    for(int i = 0; i<10; i++){
+        ss << "Reserved2[" << i << "]                : " << setw(4) << dos_header.e_res2[i] << "\r\n";
+    }
+    
+    ss << "Offset to New EXE Header     : " << setw(8) << dos_header.e_lfanew << "\r\n";
+    ss << "------------------------------------" << "\r\n";
+
+    return ss.str();
+}
+
+string GetNtHeaderString(string filepath){
+    ifstream fin(filepath, ios::binary);
+    if (!fin) return "Error: Cannot open file.";
+
+    IMAGE_DOS_HEADER dos_header;
+    fin.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
+
+    if (dos_header.e_magic != IMAGE_DOS_SIGNATURE) return "Error: Invalid DOS Signature.";
+
+    // NT Header 위치로 이동
+    IMAGE_NT_HEADERS32 nt_header;
+    fin.seekg(dos_header.e_lfanew, ios::beg);
+    fin.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS32));
+
+    if (nt_header.Signature != IMAGE_NT_SIGNATURE) return "Error: Invalid NT Signature.";
+
+    stringstream ss;
+    ss << "========== NT HEADER ==========\r\n\r\n";
+    ss << hex << uppercase << setfill('0');
+
+    ss << "[File Header]\r\n";
+    ss << "Machine              : " << setw(4) << nt_header.FileHeader.Machine << "\r\n";
+    ss << "Number of Sections   : " << setw(4) << nt_header.FileHeader.NumberOfSections << "\r\n";
+    ss << "Time Date Stamp      : " << setw(8) << nt_header.FileHeader.TimeDateStamp << "\r\n";
+    ss << "Ptr to Symbol Table  : " << setw(8) << nt_header.FileHeader.PointerToSymbolTable << "\r\n";
+    ss << "Size of Optional Hdr : " << setw(4) << nt_header.FileHeader.SizeOfOptionalHeader << "\r\n";
+    ss << "Characteristics      : " << setw(4) << nt_header.FileHeader.Characteristics << "\r\n\r\n";
+
+    ss << "[Optional Header]\r\n";
+    ss << "Magic                : " << setw(4) << nt_header.OptionalHeader.Magic << " (PE32)\r\n";
+    ss << "Address of Entry Pnt : " << setw(8) << nt_header.OptionalHeader.AddressOfEntryPoint << "\r\n";
+    ss << "Image Base           : " << setw(8) << nt_header.OptionalHeader.ImageBase << "\r\n";
+    ss << "Section Alignment    : " << setw(8) << nt_header.OptionalHeader.SectionAlignment << "\r\n";
+    ss << "File Alignment       : " << setw(8) << nt_header.OptionalHeader.FileAlignment << "\r\n";
+    ss << "Size of Image        : " << setw(8) << nt_header.OptionalHeader.SizeOfImage << "\r\n";
+    ss << "Size of Headers      : " << setw(8) << nt_header.OptionalHeader.SizeOfHeaders << "\r\n";
+    ss << "Subsystem            : " << setw(4) << nt_header.OptionalHeader.Subsystem << "\r\n";
+    ss << "Num of Data Direct.  : " << setw(8) << nt_header.OptionalHeader.NumberOfRvaAndSizes << "\r\n";
+    ss << "-------------------------------";
+
+    return ss.str();
+}
+
+string GetSectionHeadersString(string filepath) {
+    ifstream fin(filepath, ios::binary);
+    if (!fin) return "Error: Cannot open file.";
+
+    IMAGE_DOS_HEADER dos_header;
+    fin.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
+
+    IMAGE_NT_HEADERS32 nt_header;
+    fin.seekg(dos_header.e_lfanew, ios::beg);
+    fin.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS32));
+
+    int numberOfSections = nt_header.FileHeader.NumberOfSections;
+    vector<IMAGE_SECTION_HEADER> sections(numberOfSections);
+
+    // 섹션 헤더는 NT 헤더 바로 뒤에 이어집니다.
+    fin.read(reinterpret_cast<char*>(sections.data()), sizeof(IMAGE_SECTION_HEADER) * numberOfSections);
+
+    stringstream ss;
+    ss << "========== SECTION HEADERS (" << dec << numberOfSections << ") ==========\r\n\r\n";
+    ss << hex << uppercase << setfill('0');
+
+    for (int i = 0; i < numberOfSections; i++) {
+        ss << "[" << dec << (i + 1) << "] Name : " << sections[i].Name << "\r\n";
+        ss << "    Virtual Size     : " << setw(8) << sections[i].Misc.VirtualSize << "\r\n";
+        ss << "    Virtual Address  : " << setw(8) << sections[i].VirtualAddress << " (RVA)\r\n";
+        ss << "    Size of Raw Data : " << setw(8) << sections[i].SizeOfRawData << "\r\n";
+        ss << "    Ptr to Raw Data  : " << setw(8) << sections[i].PointerToRawData << "\r\n";
+        ss << "    Characteristics  : " << setw(8) << sections[i].Characteristics << "\r\n";
+        ss << "------------------------------------------\r\n";
+    }
+
+    return ss.str();
+}
+
+string GetIATString(string filepath){
+    ifstream fin(filepath, ios::binary);
+    if (!fin) return "Error: Cannot open file.";
+
+    IMAGE_DOS_HEADER dos_header;
+    fin.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
+
+    IMAGE_NT_HEADERS32 nt_header;
+    fin.seekg(dos_header.e_lfanew, ios::beg);
+    fin.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS32));
+
+    int numberOfSections = nt_header.FileHeader.NumberOfSections;
+    vector<IMAGE_SECTION_HEADER> sections(numberOfSections);
+    fin.read(reinterpret_cast<char*>(sections.data()), sizeof(IMAGE_SECTION_HEADER) * numberOfSections);
+
+    IMAGE_DATA_DIRECTORY import_dir = nt_header.OptionalHeader.DataDirectory[1];
+    if (import_dir.VirtualAddress == 0) return "Import Table not found.";
+
+    DWORD import_table_raw = RVAToRAW(import_dir.VirtualAddress, sections);
+    if (import_table_raw == 0) return "Error: Invalid Import Table Address.";
+
+    stringstream ss;
+    ss << "========== IMPORT ADDRESS TABLE (IAT) ==========\r\n\r\n";
+
+    fin.seekg(import_table_raw, ios::beg);
+    IMAGE_IMPORT_DESCRIPTOR iid;
+    DWORD current_descriptor_pos = import_table_raw;
+
+    while (true) {
+        fin.seekg(current_descriptor_pos, ios::beg);
+        fin.read(reinterpret_cast<char*>(&iid), sizeof(iid));
+        
+        if (iid.Name == 0 && iid.OriginalFirstThunk == 0) break;
+
+        string dll_name = "Unknown";
+        DWORD name_raw = RVAToRAW(iid.Name, sections);
+        if (name_raw != 0) {
+            fin.seekg(name_raw, ios::beg);
+            getline(fin, dll_name, '\0');
+        }
+
+        ss << "[ DLL: " << dll_name << " ]\r\n";
+        ss << "------------------------------------------------------------\r\n";
+        ss << left << setw(12) << "RVA" << setw(12) << "Data" << "Function Name / Ordinal" << "\r\n";
+        ss << "------------------------------------------------------------\r\n";
+
+        DWORD thunk_rva = (iid.OriginalFirstThunk != 0) ? iid.OriginalFirstThunk : iid.FirstThunk;
+        DWORD thunk_raw = RVAToRAW(thunk_rva, sections);
+        
+        if (thunk_raw != 0) {
+            IMAGE_THUNK_DATA32 thunk;
+            DWORD current_thunk_pos = thunk_raw;
+
+            while (true) {
+                fin.seekg(current_thunk_pos, ios::beg);
+                fin.read(reinterpret_cast<char*>(&thunk), sizeof(thunk));
+
+                if (thunk.u1.AddressOfData == 0) break;
+
+                ss << hex << uppercase << setw(12) << thunk_rva << setw(12) << thunk.u1.AddressOfData;
+
+                if (IMAGE_SNAP_BY_ORDINAL32(thunk.u1.Ordinal)) {
+                    ss << "Ordinal: " << dec << (thunk.u1.Ordinal & 0xFFFF) << "\r\n";
+                } else {
+                    DWORD func_name_raw = RVAToRAW(thunk.u1.AddressOfData, sections);
+                    if (func_name_raw != 0) {
+                        fin.seekg(func_name_raw + 2, ios::beg);
+                        string func_name;
+                        getline(fin, func_name, '\0');
+                        ss << func_name << "\r\n";
+                    } else {
+                        ss << "Invalid Name RVA\r\n";
+                    }
+                }
+                current_thunk_pos += sizeof(IMAGE_THUNK_DATA32);
+                thunk_rva += sizeof(IMAGE_THUNK_DATA32);
+            }
+        }
+        ss << "\r\n";
+        current_descriptor_pos += sizeof(IMAGE_IMPORT_DESCRIPTOR);
+    }
+    
+    return ss.str();
+}
+
+string GetEATString(string filepath){
+    ifstream fin(filepath, ios::binary);
+    if (!fin) return "Error: Cannot open file.";
+
+    IMAGE_DOS_HEADER dos_header;
+    fin.read(reinterpret_cast<char*>(&dos_header), sizeof(IMAGE_DOS_HEADER));
+    IMAGE_NT_HEADERS32 nt_header;
+    fin.seekg(dos_header.e_lfanew, ios::beg);
+    fin.read(reinterpret_cast<char*>(&nt_header), sizeof(IMAGE_NT_HEADERS32));
+    
+    int numberOfSections = nt_header.FileHeader.NumberOfSections;
+    vector<IMAGE_SECTION_HEADER> sections(numberOfSections);
+    fin.read(reinterpret_cast<char*>(sections.data()), sizeof(IMAGE_SECTION_HEADER) * numberOfSections);
+
+    IMAGE_DATA_DIRECTORY export_dir = nt_header.OptionalHeader.DataDirectory[0];
+    if (export_dir.VirtualAddress == 0) return "Export Table not found (This file may not be a DLL).";
+
+    DWORD eat_raw = RVAToRAW(export_dir.VirtualAddress, sections);
+    if (eat_raw == 0) return "Error: Invalid Export Table Address.";
+
+    IMAGE_EXPORT_DIRECTORY eat;
+    fin.seekg(eat_raw, ios::beg);
+    fin.read(reinterpret_cast<char*>(&eat), sizeof(eat));
+
+    string dll_name = "Unknown";
+    DWORD name_raw = RVAToRAW(eat.Name, sections);
+    if (name_raw != 0) {
+        fin.seekg(name_raw, ios::beg);
+        getline(fin, dll_name, '\0');
+    }
+
+    stringstream ss;
+    ss << "========== EXPORT ADDRESS TABLE (EAT) ==========\r\n\r\n";
+    ss << "DLL Name           : " << dll_name << "\r\n";
+    ss << "Base               : " << eat.Base << "\r\n";
+    ss << "Number of Functions: " << eat.NumberOfFunctions << "\r\n";
+    ss << "Number of Names    : " << eat.NumberOfNames << "\r\n";
+    ss << "Address of Funcs   : " << hex << uppercase << eat.AddressOfFunctions << "\r\n";
+    ss << "Address of Names   : " << eat.AddressOfNames << "\r\n\r\n";
+
+    ss << "--------------------------------------------------\r\n";
+    ss << left << setw(10) << "Ordinal" << setw(12) << "RVA" << "Function Name" << "\r\n";
+    ss << "--------------------------------------------------\r\n";
+
+    if (eat.NumberOfNames > 0 && eat.AddressOfNames != 0) {
+        DWORD names_start_raw = RVAToRAW(eat.AddressOfNames, sections);
+        DWORD ordinals_start_raw = RVAToRAW(eat.AddressOfNameOrdinals, sections);
+        DWORD func_start_raw = RVAToRAW(eat.AddressOfFunctions, sections);
+
+        for (DWORD i = 0; i < eat.NumberOfNames; i++) {
+            DWORD name_rva;
+            fin.seekg(names_start_raw + i * 4, ios::beg);
+            fin.read(reinterpret_cast<char*>(&name_rva), sizeof(name_rva));
+
+            WORD ordinal;
+            fin.seekg(ordinals_start_raw + i * 2, ios::beg);
+            fin.read(reinterpret_cast<char*>(&ordinal), sizeof(ordinal));
+
+            DWORD func_rva;
+            fin.seekg(func_start_raw + ordinal * 4, ios::beg); 
+            fin.read(reinterpret_cast<char*>(&func_rva), sizeof(func_rva));
+
+            string func_name;
+            DWORD str_raw = RVAToRAW(name_rva, sections);
+            if (str_raw != 0) {
+                fin.seekg(str_raw, ios::beg);
+                getline(fin, func_name, '\0');
+            }
+
+            ss << hex << uppercase << setw(10) << (eat.Base + ordinal) << setw(12) << func_rva << func_name << "\r\n";
+        }
+    }
+
+    return ss.str();
+}
+
 // 윈도우 메시지 처리 함수 (이벤트 처리기)
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
@@ -63,7 +366,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 MessageBoxA(hwnd, "Open File first", "alert", MB_OK | MB_ICONINFORMATION);
             }
             else{
-                SetWindowTextA(hEdit, "View -> DOS Header 메뉴가 클릭되었습니다.\r\n여기에 DOS 헤더 정보가 출력됩니다.");
+                string res = GetDosHeaderString(cur_Filepath);
+                SetWindowTextA(hEdit, res.c_str());
             }
             break;
         case ID_VIEW_NT:
@@ -71,7 +375,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 MessageBoxA(hwnd, "Open File first", "alert", MB_OK | MB_ICONINFORMATION);
             }
             else{
-                SetWindowTextA(hEdit, "View -> NT Header 메뉴가 클릭되었습니다.");
+                string result = GetNtHeaderString(cur_Filepath);
+                SetWindowTextA(hEdit, result.c_str());
             }
             break;
         case ID_FILE_EXIT:
@@ -82,7 +387,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 MessageBoxA(hwnd, "Open File first", "alert", MB_OK | MB_ICONINFORMATION);
             }
             else{
-                SetWindowTextA(hEdit, "View -> Section Headers 메뉴가 클릭되었습니다.\r\n섹션 정보가 여기에 출력됩니다.");
+                string result = GetSectionHeadersString(cur_Filepath);
+                SetWindowTextA(hEdit, result.c_str());
             }
             break;
         case ID_VIEW_IAT:
@@ -90,7 +396,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 MessageBoxA(hwnd, "Open File first", "alert", MB_OK | MB_ICONINFORMATION);
             }
             else{
-                SetWindowTextA(hEdit, "View -> Import Table (IAT) 메뉴가 클릭되었습니다.");
+                string result = GetIATString(cur_Filepath);
+                SetWindowTextA(hEdit, result.c_str());
             }
             break;
         case ID_VIEW_EAT:
@@ -98,7 +405,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 MessageBoxA(hwnd, "Open File first", "alert", MB_OK | MB_ICONINFORMATION);
             }
             else{
-                SetWindowTextA(hEdit, "View -> Export Table (EAT) 메뉴가 클릭되었습니다.");
+                string result = GetEATString(cur_Filepath);
+                SetWindowTextA(hEdit, result.c_str());
             }
             break;
         }
